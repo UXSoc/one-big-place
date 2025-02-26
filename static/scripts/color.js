f6605bdf5b1a72bd9aa3678beb3573913fa221c8
Chesca <fdjr401@gmail.com>
2025-02-26 21:31:08 +0800

    Add temporary color palette

---
+++ static/scripts/color.js
@@ -0,0 +1,6 @@
+const colorButton = document.getElementById("color-button");
+
+colorButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  console.log("color button was clicked!");
+})

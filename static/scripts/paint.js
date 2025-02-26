4c2b0c1d4d44003f45be8f0ada0853a30d4b8ac8
Chesca <fdjr401@gmail.com>
2025-02-26 22:30:58 +0800

    Add timer to pixel paint

--- static/scripts/paint.js
+++ static/scripts/paint.js
@@ -1,10 +1,20 @@
+let timerLength = 5000; // in ms
+let isTimerRunning = false;
+
 export function paintPixel(color, coords) {
   // get current canvas
   const canvas = document.querySelector(".panner-container canvas");
   const ctx = canvas.getContext("2d");
 
-  // paint the pixel onto the canvas
+  // paint the pixel onto the canvas (only if timer isn't running)
   // will need to wait for the canvas to load
-  ctx.fillStyle = color;
-  ctx.fillRect(coords.x, coords.y, 1, 1);
+  if (isTimerRunning == false) {
+    ctx.fillStyle = color;
+    ctx.fillRect(coords.x, coords.y, 1, 1);
+    isTimerRunning = true;
+    setTimeout(() => {
+      isTimerRunning = false;
+    }, timerLength);
+  }
+
 }

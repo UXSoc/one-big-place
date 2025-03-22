d2de33374b5f73c082dd3df72b9f57df0176c028
Angelo-Funelas <angelomartinfunelas@gmail.com>
2025-03-22 22:00:49 +0800

    Added bits display

--- static/scripts/paint.js
+++ static/scripts/paint.js
@@ -12,6 +12,7 @@ function generatePixl() {
     } else {
       generationTimer = undefined
     }
+    updateBits()
   }, slotGenerationCooldown)
 }
 
@@ -26,8 +27,35 @@ export function paintPixel(color, coords) {
     ctx.fillStyle = color;
     ctx.fillRect(coords.x, coords.y, 1, 1);
     slotCount--;
+    updateBits()
     if (!generationTimer) {
       generationTimer = generatePixl();
     }
   }
 }
+
+const bits_container = document.querySelector("#bits-counter");
+const bits_p = document.querySelector("#bits-display > div > p")
+function loadBits() {
+  for (let i = 0; i < slotCount; i++) {
+    var bit = document.createElement('div')
+    bit.classList.add("filled")
+    bits_container.append(bit)
+  }
+  bits_p.innerHTML = "<span>0</span>Bits Left"
+}
+loadBits()
+
+function updateBits() {
+  var bits = bits_container.querySelectorAll("div");
+  console.log(bits)
+  for (let i = 0; i < bits.length; i++) {
+    if (i > slotCount-1) {
+      bits[i].classList.remove("filled");
+    } else {
+      bits[i].classList.add("filled");
+    }
+  }
+  bits_p.innerHTML = `<span>${slotCount}</span> Bits Left`
+}
+updateBits()

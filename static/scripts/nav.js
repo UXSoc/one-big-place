c9376ea4f128d697e99843e035228c5830b85a99
Angelo-Funelas <angelomartinfunelas@gmail.com>
2025-03-23 17:05:39 +0800

    Implemented close modals on outside click

--- static/scripts/nav.js
+++ static/scripts/nav.js
@@ -1,4 +1,6 @@
-var modals = [
+const NAV_ITEMS = document.querySelectorAll("nav > ul > li")
+const NAV = document.querySelector("nav")
+const MODALS = [
     "leaderboard",
     "challenges",
     "coverage",
@@ -6,22 +8,36 @@ var modals = [
     "credits",
 ]
 
-const nav_items = document.querySelectorAll("nav > ul > li")
-const nav = document.querySelector("nav")
-for (let i = 0; i < nav_items.length; i++) {
-    nav_items[i].addEventListener('click', function() {
-        console.log("click")
-        document.querySelectorAll(".modal").forEach((item) => {
-            item.remove();
-        })
-        fetch(`modals/${modals[i]}.html`) 
+function closeModals() {
+    document.querySelectorAll(".modal").forEach((item) => {
+        item.remove();
+    })
+    NAV.classList.remove("active")
+    NAV_ITEMS.forEach((item) => {
+        item.classList.remove("active");
+    })
+}
+
+
+document.addEventListener("click", (e) => {
+    if (!NAV.contains(e.target)) {
+        closeModals();
+    }
+})
+
+for (let i = 0; i < NAV_ITEMS.length; i++) {
+    NAV_ITEMS[i].addEventListener('click', function() {
+        closeModals();
+        NAV.classList.add("active")
+        NAV_ITEMS[i].classList.add("active")
+        fetch(`modals/${MODALS[i]}.html`) 
         .then(response => response.text()) 
         .then(html => { 
             var modal = document.createElement("div")
             modal.className = "modal"
-            modal.style.maxHeight = `${nav.offsetHeight}px`;
+            modal.style.maxHeight = `${NAV.offsetHeight}px`;
             modal.innerHTML = html
-            nav.appendChild(modal); 
+            NAV.appendChild(modal);
         }) 
         .catch(error => console.error('Error loading HTML:', error)); 
     });

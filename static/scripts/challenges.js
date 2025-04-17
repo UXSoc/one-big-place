6ccc71e467fceee12826c8cfe3f37b1c2e2ccc59
Arthur Lluisma <ajclluisma@gmail.com>
2025-04-17 17:45:52 +0800

    Add additional challenges and edit font sizes of tabs

--- static/scripts/challenges.js
+++ static/scripts/challenges.js
@@ -1,13 +1,53 @@
-const DUMMY_CHALLENGES = [
+const CHALLENGES = [
     {
         name: "Social Butterfly",
-        increase: "1",
+        increase: 1,
         description: "Share the event announcement post on Facebook."
+    },
+    {
+        name: "Tweet Master",
+        increase: 1,
+        description: "Share the event link on X/Twitter."
+    },
+    {
+        name: "#1 Fan",
+        increase: 1,
+        description: "Follow the User Experience Society Facebook Page."
+    },
+    {
+        name: "Tara Gonz",
+        increase: 1,
+        description: "Place 120 total pixels during lunchtime (between 11:00am - 2:00pm)."
+    },
+    {
+        name: "Excuse Me",
+        increase: 1,
+        description: "Replace 150 pixels placed by a user."
+    },
+    {
+        name: "Starter Sketcher",
+        increase: 1,
+        description: "Place 200 pixels."
+    },
+    {
+        name: "Master Painter",
+        increase: 1,
+        description: "Place 500 pixels."
+    },
+    {
+        name: "Artistic Virtuouso",
+        increase: 1,
+        description: "Place 1000 pixels."
+    },
+    {
+        name: "Grandmaster Artisan",
+        increase: 1,
+        description: "Place 5000 pixels."
     }
 ];
 
 let challenges = document.querySelector(".challenges");
-for (const challenge of DUMMY_CHALLENGES) {
+for (const challenge of CHALLENGES) {
     let div = document.createElement("div");
     let divFirst = document.createElement("div");
     let divSecond = document.createElement("div");

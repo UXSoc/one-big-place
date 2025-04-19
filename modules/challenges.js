11bfbfba3fcdfcec9be8996f8a4d1adbab1485d4
Angelo-Funelas <angelomartinfunelas@gmail.com>
2025-04-20 06:36:01 +0800

    Fixed issue where link challenges could not be completed

--- modules/challenges.js
+++ modules/challenges.js
@@ -26,7 +26,7 @@ function isChallengeCompleted(user, code) {
     if (!isChallenge(code)) throw new Error(`${code} is not a challenge.`);
     code = code.slice(10);
     const challenge = challenges.find(ch => ch.id === code);
-    if (challenge["type"] !== "progress") throw new Error(`${challenge.id} is not a progress challenge.`)
+    if (challenge["type"] !== "progress") return true;
     const total = challenge["required"];
     const current = user[challenge["field"]];
     return (current>=total);
